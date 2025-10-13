85193c9d4b04f57dec94cb106300b75930181140
abrka <ifradnashit@gmail.com>
2025-10-13 13:11:25 +0600

    add a function to create arbitary plot data for gnuplot

--- include/mfo/neural_net_wrapper.h
+++ include/mfo/neural_net_wrapper.h
@@ -52,4 +52,22 @@ void nn_add_layer(nn_arena_t *arena, nn_network_t *net, size_t num_allocated_lay
   nn_push_layer(net, num_allocated_layers, created_layer);
 }
 
+void nn_create_y_vs_x_plot(nn_network_t *net, char *str_buf, size_t str_buf_size, double x_min, double x_max, size_t num_points)
+{
+  char *current_line = str_buf;
+  for (size_t i = 0; i < num_points; i++)
+  {
+    double x = map_range((double)i, 0.0, (double)num_points, x_min, x_max);
+    double y = -1;
+    nn_predict(net, &x, 1, &y, 1);
+
+    size_t tot_chars_written = (current_line - str_buf);
+    assert(tot_chars_written < str_buf_size);
+    size_t chars_left_in_str_buf = (str_buf_size - (current_line - str_buf));
+    int chars_written = snprintf(current_line, chars_left_in_str_buf, "%g %g\n ", x, y);
+    assert(chars_written > 0);
+    current_line += chars_written - 1;
+  }
+}
+
 #endif
